02755a2df37dcb34a5240cfdd41dbca5d972ab87
Ushakov Artem <artom444fta@yandex.ru>
2025-05-04 20:44:28 +0300

    main: added styles and flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,16 +3,48 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"flag"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "Сетевой адрес HTTP")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/{id}", showSnippet)
 	mux.HandleFunc("POST /snippet/create", createSnippet)
 
-	port := ":8080"
-	log.Println("Запуск сервера на http://localhost:8080")
-	err := http.ListenAndServe(port, mux)
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Запуск сервера на %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
